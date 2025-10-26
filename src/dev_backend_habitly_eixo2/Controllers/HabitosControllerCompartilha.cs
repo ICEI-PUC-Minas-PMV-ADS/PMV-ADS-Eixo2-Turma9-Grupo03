05f53be6d54b094a95b1ca64d11abc681324bbe5
pedrord7 <gomes.rd7@gmail.com>
2025-10-26 12:04:08 -0300

    Rf-013/RF-014
    Feito compartilha Habito e model de Preferencia de usuarios

---
+++ src/dev_backend_habitly_eixo2/Controllers/HabitosControllerCompartilha.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using dev_backend_habitly_eixo2.Models;
+
+
+namespace dev_backend_habitly_eixo2.Controllers
+{
+    public class HabitosControllerCompartilha : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HabitosControllerCompartilha(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Habitos
+        public IActionResult Index()
+        {
+            var habitos = _context.Habitos.ToList();
+            return View(habitos);
+        }
+    }
+}
---
+++ src/dev_backend_habitly_eixo2/Migrations/20251026134707_AddPreferenciasUsuario.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dev_backend_habitly_eixo2.Migrations
+{
+    public partial class AddPreferenciasUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PreferenciasUsuario",
+                columns: table => new
+                {
+                    IdPreferencia = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false),
+                    Tema = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NotificacoesAtivas = table.Column<bool>(type: "bit", nullable: false),
+                    Idioma = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PreferenciasUsuario", x => x.IdPreferencia);
+                    table.ForeignKey(
+                        name: "FK_PreferenciasUsuario_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "IdUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PreferenciasUsuario_IdUsuario",
+                table: "PreferenciasUsuario",
+                column: "IdUsuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PreferenciasUsuario");
+        }
+    }
+}
